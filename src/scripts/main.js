30952c751cad5725ffe1833b5a5833e8f2aa30c7
Henrique <Henriqueb96@outlook.com>
2024-03-01 11:22:34 -0300

    subindo os arquivos footer, hero, event

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+const dataDoAniversario = new Date ("Mar 06, 2024 19:00:00");
+const timeStampAniversario = dataDoAniversario.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOAniversario = timeStampAniversario - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOAniversario = Math.floor(distanciaAteOAniversario / diaEmMs)
+    const horasAteOAniversario = Math.floor((distanciaAteOAniversario % diaEmMs) / horaEmMs);
+    const minutosAteOAniversario = Math.floor((distanciaAteOAniversario % horaEmMs) / minutoEmMs);
+    const segundosAteOAniversario = Math.floor((distanciaAteOAniversario % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOAniversario}d ${horasAteOAniversario}h ${minutosAteOAniversario}m ${segundosAteOAniversario}s`;
+
+    if (distanciaAteOAniversario < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado'
+    }
+},1000)
+
